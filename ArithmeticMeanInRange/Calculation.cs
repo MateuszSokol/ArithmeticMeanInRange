b92052c088873fb3ad8f8d60cf14c5c7f42bc1a8
mateusz1231 <matizek2@vp.pl>
2022-01-25 09:25:03 +0100

    initial commit

---
+++ ArithmeticMeanInRange/Calculation.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace ArithmeticMeanInRange
+{
+   public class Calculation
+    {
+        static void Main(string[] args)
+        {
+
+
+            //int[] arr = { 1, 2, 3, 4, 5, 6 };
+            int r = 2;
+            int[] arr = { 1, 2, 3, 4, 5 };
+            Calculation.SearchValueInArr(arr,2);
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+      
+        public static int[] SearchValueInArr(int[] arr, int range)
+        {
+            int sum, counter;
+            int divideCounter = 0;
+            int [] returnValueArr = new int [arr.Length];
+
+           
+            for (int i = 0; i < arr.Length; i++)              
+            {
+                counter = 0;
+                sum = 0;
+
+
+                
+                if (i-range < 0)
+                {
+                    for (int h = 0; h < range; h++)
+                    {
+
+
+
+                        sum += arr[h];
+                        counter++;
+                    }
+                }
+                else {
+
+                    for (int c = (i-range)+1; c < i; c++)
+                    {
+                        sum += arr[c];
+                        counter++;
+                    }
+                }
+
+                if (i + range < arr.Length)
+                {
+                    for (int g = range; g < range+i; g++)
+                    {
+                        sum += arr[g];
+                        counter++;
+                    }
+                }
+                else
+                {
+                    for (int k = i; k < arr.Length; k++)
+                    {
+                        sum += arr[k];
+                        counter++;
+                    }
+                }
+                    
+                returnValueArr[i] = sum/counter;
+                
+
+
+               
+            }
+            return returnValueArr;
+        
+        }
+    }
+ 
+}
---
+++ ArithmeticMeanInRangeTest/CalculationTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ArithmeticMeanInRange;
+
+
+namespace ArithmeticMeanInRangeTest
+{
+    public class CalculationTest
+    {
+        [Fact]
+        public void ShouldReturn_SameArrExcept_LastNumberEqual_5()
+        {
+
+            Assert.Equal(Calculation.SearchValueInArr(new[] { 1, 2, 3, 4, 5 }, 2), new[] { 1, 2, 3, 4, 4 });
+
+        }
+        [Fact]
+
+        public void ShouldReurunArr()
+        {
+            Assert.Equal(Calculation.SearchValueInArr(new[] {6,9,12,5,3,4}, 3), new[] {9, 8, 7, 6, 6, 4});
+        }
+    }
+}
